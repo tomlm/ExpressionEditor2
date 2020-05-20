47fca20a0ac89060dc53e11d1931931db6a2b884
Tom Laird-McConnell <tomlm@microsoft.com>
2020-05-20 12:11:19 -0700

    upgrade to latest 3.2 Blazor

--- ExpressionEditor2/Program.cs
+++ ExpressionEditor2/Program.cs
@@ -1,16 +1,46 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Iciclecreek.Bot.Expressions.Humanizer;
+using BlazorStyled;
 
 namespace ExpressionEditor2
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services
+                 .AddBlazorise(options =>
+                 {
+                     options.ChangeTextOnKeyPress = true;
+                 })
+                 .AddBootstrapProviders()
+                 .AddFontAwesomeIcons()
+                 .AddBlazorStyled();
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            builder.RootComponents.Add<MyApp>("myApp");
+
+            builder.Services.AddSingleton(sp => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+
+             // componentsApplicationBuilder.AddComponent<ClientSideStyled>("#styled");
+
+            HumanizerFunctions.Register();
+            var host = builder.Build();
+            host.Services
+                .UseBootstrapProviders()
+                .UseFontAwesomeIcons();
+
+            await host.RunAsync();
+        }
     }
 }
