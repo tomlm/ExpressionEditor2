92ff8c4d8563e99e21ee136f9cad36f9358cfa5c
Tom Laird-McConnell <tomlm@microsoft.com>
2020-06-10 11:46:28 -0700

    bump version

--- ExpressionEditor2/wwwroot/ServiceWorker.js
+++ ExpressionEditor2/wwwroot/ServiceWorker.js
@@ -5,7 +5,7 @@ const swInstallEvent = 'install';
 const swInstalledEvent = 'installed';
 const swActivateEvent = 'activate';
 const staticCachePrefix = 'blazor-cache-v';
-const staticCacheName = 'blazor-cache-v13';
+const staticCacheName = 'blazor-cache-v14';
 const requiredFiles = [
 "/css/bootstrap/bootstrap.min.css",
 "/css/bootstrap/bootstrap.min.css.map",
